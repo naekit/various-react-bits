6456ad8b50cfda6fce2d5dc65c52d60a50fad309
naekit <mykyta.onishchuck@code.berlin>
2023-04-30 17:32:00 -0400

    finished grid challenge

--- src/components/Memory.tsx
+++ src/components/Memory.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react"
 
+type TCell = {
+	row: number
+	col: number
+}
+
 function Memory() {
 	const [grid, setGrid] = useState([
 		[0, 3, 5, 1],
@@ -13,16 +18,43 @@ function Memory() {
 			.map(() => new Array(grid[0].length).fill(false))
 	)
 
-	function handleCardClicked(rowIndex: number, colIndex: number) {
-		const newRevealedGrid = [...revealedGrid]
-		newRevealedGrid[rowIndex][colIndex] =
-			!newRevealedGrid[rowIndex][colIndex]
-		setRevealedGrid(newRevealedGrid)
-		// reveal clicked card
+	const [prevClicked, setPrevClick] = useState<TCell | undefined>()
 
+	function handleCardClicked(rowIndex: number, colIndex: number) {
+		if (revealedGrid[rowIndex][colIndex]) return
+		// reveal clicked card
+		const clickedNum = grid[rowIndex][colIndex]
+		const newRevealedGrid = [...revealedGrid]
+		newRevealedGrid[rowIndex][colIndex] = true
+		setRevealedGrid(newRevealedGrid)
+		// setPrevClick(clickedNum)
 		// if one card has already been clicked prior
-		// if they both match, mark them as answered
-		// if they don't match, hide them after 1 second
+		if (prevClicked) {
+			const prevClickedNum = grid[prevClicked?.row][prevClicked?.col]
+			// second click of 2 clicks
+			// if they both match, mark them as answered
+			if (prevClickedNum !== clickedNum) {
+				setTimeout(() => {
+					newRevealedGrid[rowIndex][colIndex] = false
+					newRevealedGrid[prevClicked.row][prevClicked.col] = false
+					setRevealedGrid([...newRevealedGrid])
+				}, 1000)
+			} else {
+				const hasWon = revealedGrid
+					.flat()
+					.every((isRevealed) => isRevealed)
+				if (hasWon) {
+					setTimeout(() => {
+						alert("You Won!!!!")
+					})
+				}
+			}
+			// if they don't match, hide them after 1 second
+			setPrevClick(undefined)
+		} else {
+			setPrevClick({ row: rowIndex, col: colIndex })
+		}
+
 		console.log(rowIndex, colIndex)
 	}
 
@@ -36,7 +68,11 @@ function Memory() {
 								handleCardClicked(rowIndex, colIndex)
 							}
 							key={colIndex}
-							className="card"
+							className={`card ${
+								revealedGrid[rowIndex][colIndex]
+									? "revealed"
+									: ""
+							}`}
 						>
 							{revealedGrid[rowIndex][colIndex] ? num : "X"}
 						</div>
