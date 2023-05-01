ac9a57c15a209a54217efc733e0f0576d0b47790
naekit <mykyta.onishchuck@code.berlin>
2023-05-01 13:40:38 -0400

    changed around imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import { useState } from "react"
 import "./App.css"
 import Memory from "./components/Memory"
 import Conditionals from "./components/Condtionals"
