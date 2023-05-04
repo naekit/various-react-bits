1a4012e89bc46c391e040ce422e2e486df9fdd1d
naekit <mykyta.onishchuck@code.berlin>
2023-05-04 10:21:53 -0400

    finished tower of hanoi visual

--- src/components/TowerOfHanoi.tsx
+++ src/components/TowerOfHanoi.tsx
@@ -1,18 +1,32 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 function TowerOfHanoi() {
-	const [towers, setTowers] = useState([[5, 4, 3, 2, 1], [], []])
+	const [towers, setTowers] = useState([[3, 2, 1], [], []])
 	const [selected, setSelected] = useState(-1)
 	const [moves, setMoves] = useState(0)
 
 	function handleClickedTower(clickedIndex: number) {
 		if (selected >= 0) {
+			if (
+				towers[clickedIndex].length > 0 &&
+				towers[clickedIndex][towers[clickedIndex].length - 1] <
+					towers[selected][towers[selected].length - 1]
+			) {
+				return
+			}
 			const newTowers = [...towers]
 			const poppedDisc = newTowers[selected].pop()
-			if (poppedDisc === undefined || newTowers.length < 0) return
+			if (poppedDisc === undefined || newTowers.length < 0) {
+				setSelected(-1)
+				return
+			}
 			newTowers[clickedIndex].push(poppedDisc)
 			setTowers(newTowers)
 			setSelected(-1)
+			setMoves(moves + 1)
+			if (newTowers[2].length === 3) {
+				alert(`You won in ${moves} moves!`)
+			}
 		} else {
 			setSelected(clickedIndex)
 		}
@@ -33,9 +47,7 @@ function TowerOfHanoi() {
 								key={i}
 								className="disc"
 								style={{ width: (v + 1) * 20 }}
-							>
-								{v}
-							</div>
+							></div>
 						))}
 					</div>
 				</div>
